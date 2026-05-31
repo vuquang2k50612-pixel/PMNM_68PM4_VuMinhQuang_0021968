aedb10d629c87d02409758ae58a09459646341b5
Antony-Taylor <vuquang2k50612@gmail.com>
2026-05-31 21:46:45 +0700

    add SinhVienModel + getAll()

---
+++ app/models/SinhVienModel.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'app/core/Database.php';
+
+class SinhVienModel{
+    private $db;
+    public function __construct(){
+        $database = new Database();
+        $this->db = $database->getConnection();
+    }
+    public function getAll(){
+        $sql = 'SELECT * FROM sinh_vien';
+        try {
+            $stmt =$this->db->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch (PDOException $e){
+            echo "Loi lay du lieu:" . $e->getMessage();
+            return[];
+        }
+    }
+}
+?>
